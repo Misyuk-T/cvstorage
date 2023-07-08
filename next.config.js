fd64568f876f95afcb0f00ff67f2e182b520bc64
Misiuk <misyuktaras@gmail.com>
2023-07-08 18:51:02 +0300

    FE: Change folder structure. Delete default next mock files

--- next.config.js
+++ next.config.js
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  pageExtensions: ["page.jsx", "page.js"],
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
