be8afdcc08d6449a1ab653094c5f1a289ce94aee
Misiuk <taras.m@team.anyforsoft.com>
2023-08-23 18:43:24 +0300

    Delete all github deploy logic
    Add debug logs to resolve problem with dev env
    
    Add debug logs to resolve problem with dev env
    
    Add debug logs to resolve problem with dev env
    
    Adjust yml file
    
    Adjust yml file
    
    Adjust yml file

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,13 +1,11 @@
-import Sidebar from "@/components/Sidebar/Sidebar";
-import ProtectedRoute from "@/components/ProtectedRoute/ProtectedRoute";
 import { Box } from "@chakra-ui/react";
 
-const Home = () => {
-  const env = process.env.BASE_URL;
+import Sidebar from "@/components/Sidebar/Sidebar";
+import ProtectedRoute from "@/components/ProtectedRoute/ProtectedRoute";
 
+const Home = () => {
   return (
     <Box color="black">
-      {env} - base url
       <ProtectedRoute>
         <Sidebar />
       </ProtectedRoute>
