afdd3362d5376530e55b28bb6cc2920b162da51f
Misiuk <taras.m@team.anyforsoft.com>
2023-08-23 17:21:32 +0300

    Adjust yml file

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,11 +1,17 @@
 import Sidebar from "@/components/Sidebar/Sidebar";
 import ProtectedRoute from "@/components/ProtectedRoute/ProtectedRoute";
+import { Box } from "@chakra-ui/react";
 
 const Home = () => {
+  const env = process.env.BASE_URL;
+
   return (
-    <ProtectedRoute>
-      <Sidebar />
-    </ProtectedRoute>
+    <Box color="black">
+      {env} - base url
+      <ProtectedRoute>
+        <Sidebar />
+      </ProtectedRoute>
+    </Box>
   );
 };
 
