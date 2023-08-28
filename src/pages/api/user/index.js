59bacbf74f19c2ef6c03a193198a18b9aaaac2eb
Misiuk <taras.m@team.anyforsoft.com>
2023-08-28 15:22:26 +0300

    Extend allowed headers and methods

--- src/pages/api/user/index.js
+++ src/pages/api/user/index.js
@@ -26,10 +26,14 @@ const handler = async (req, res) => {
   switch (method) {
     case "OPTIONS":
       res.setHeader("Access-Control-Allow-Origin", "*");
-      res.setHeader("Access-Control-Allow-Methods", "GET, POST");
+      res.setHeader(
+        "Access-Control-Allow-Methods",
+        "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+      );
+      res.setHeader("Access-Control-Allow-Credentials", "true");
       res.setHeader(
         "Access-Control-Allow-Headers",
-        "Authorization, Content-Type",
+        "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization, Accept",
       );
       res.status(204).end();
       break;
